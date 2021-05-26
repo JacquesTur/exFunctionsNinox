1fa4b6c755bd1384834fb43f06976a6839087397
JacquesTur <jacques.tur@neuf.fr>
2021-05-26 16:00:27 +0200

    master

--- exFonctions.js
+++ exFonctions.js
@@ -1,6 +1,6 @@
 var revision = 'rev 0.00.00.17f';
 
-//import * as monModule from '/Modules/exUtils.js';
+import * as exUtils from '/Modules/exUtils.js';
 
 window.exFunctions = (function () {
     debugger;
