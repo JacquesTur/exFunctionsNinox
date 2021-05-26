56feade17606ea4307e24ba98aa0c516cd1b7544
JacquesTur <jacques.tur@neuf.fr>
2021-05-26 15:45:24 +0200

    master

--- exFonctions.js
+++ exFonctions.js
@@ -1,6 +1,6 @@
 var revision = 'rev 0.00.00.17f';
 
-
+import * as monModule from '/modules/exUtils.js';
 
 window.exFunctions = (function () {
     //debugger;
@@ -26,86 +26,7 @@ window.exFunctions = (function () {
         }
     }]);
 
-    //function utilitaires
-    function getId(record) {
-        if (typeof record === 'string') return record;
-        else if (typeof record === 'object') return record._id;
-        else return null;
-    }
 
-    function fireEval(fn, recordId) {
-
-
-        try {
-            var type = database.typeOf(recordId);
-            var compile = queries.parseHuman(database.schema, type, unescape(fn), {});
-            //            compile.flags ^= 16;
-            //            compile.flags |= 16;
-            if (compile.hasErrors()) return 'Erreur d\'expression : ' + compile.errorMessage();
-            var result = database.loadNode(recordId, (function (e, i) {
-                return e ? 'Failed to load record: ' + e : i ? void compile.evaluate(database, i, (function (error, t) {
-                    if (error)
-                        return 'Failed to evaluate expression: ' + error;
-                    return t;
-                })) : 'Record not found: ' + recordId;
-            }));
-        } catch (err) {
-            var msgErr = err.message + ' à la ligne ' + err.line + ', colonne ' + err.column;
-
-            return msgErr;
-        }
-        return result;
-    }
-
-    function fireEvalGlobal(fn) {
-
-        var result = '';
-        try {
-            var compile = queries.parseHuman(database.schema, null, unescape(fn), {});
-
-            if (compile.hasErrors()) return 'Erreur d\'expression : ' + compile.errorMessage();
-            compile.evaluate(database, null, (function (error, t) {
-                if (error)
-                    return 'Failed to evaluate expression: ' + error;
-                result = t;
-                return t;
-            }));
-        } catch (err) {
-            var msgErr = err.message + ' à la ligne ' + err.line + ', colonne ' + err.column;
-
-            return msgErr;
-        }
-        return result;
-
-    }
-
-    function fireExp(exp, recordId) {
-        try {
-            if (exp.hasErrors()) return 'Erreur d\'expression : ' + exp.errorMessage();
-            var result = database.loadNode(recordId, (function (e, i) {
-                return e ? 'Failed to load record: ' + e : i ? void exp.evaluate(database, i, (function (error, t) {
-                    if (error)
-                        return 'Failed to evaluate expression: ' + error;
-                    return t;
-                })) : 'Record not found: ' + recordId;
-            }));
-        } catch (err) {
-            var msgErr = err.message + ' à la ligne ' + err.line + ', colonne ' + err.column;
-
-            return msgErr;
-        }
-        return result;
-    }
-
-    function generateUniqueId(prefix) {
-        var id = prefix.toString();
-        var num = 0;
-        while (document.getElementById(id)) {
-            num++;
-            id = prefix.toString() + num.toString();
-        }
-        return id;
-    }
 
     //Début des fonctions étendues
     function exAlert(fnt, params, db, ret) {
