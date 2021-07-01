91d56ac997bca06125418ac998f429e6ca9ef048
JacquesTur <jacques.tur@neuf.fr>
2021-07-01 13:33:56 +0200

    master

--- Jacques/Exemple manipulation JSON.js
+++ Jacques/Exemple manipulation JSON.js
@@ -1,7 +1,23 @@
- window.database.originalSchema.types.G.fields.L1 = window.database.originalSchema.types.G.fields.N1;
- delete window.database.originalSchema.types.G.fields.N1;
- window.database.setSchema(window.database.originalSchema);
+window.database.originalSchema.types.G.fields.L1 =
+  window.database.originalSchema.types.G.fields.N1;
+delete window.database.originalSchema.types.G.fields.N1;
+window.database.setSchema(window.database.originalSchema);
 
-window.database.schema.types.G.fields.L1 = window.database.schema.types.G.fields.N1;
+window.database.schema.types.G.fields.L1 =
+  window.database.schema.types.G.fields.N1;
 delete window.database.schema.types.G.fields.N1;
- window.database.saveSchema();
+window.database.saveSchema();
+
+
+var field;
+var type;
+
+// récupération de la table (type) et du champ (field)
+if (args.record) {
+  type = database.schema.typeOf(args.record._id);
+  field = type.findElement(args.multiField);
+  if (field) {
+    args.record[field.id] = util.multiEncode(args.id);
+    return args.record[field.id];
+  } else return "Le champ "+ args.multiFiled+ " esr introuvable dans la table " + type.caption;
+} else return "Aucun enregistrement définit dans args.record";
--- Modules/exFnJt.js
+++ Modules/exFnJt.js
@@ -1,203 +1,236 @@
 window.exFnJt = (function () {
+  function exAlert(fnt, params, db, ret) {
+    alert(params.titre + "<br><br>" + params.message);
+    ret();
+  }
 
-    function exAlert(fnt, params, db, ret) {
-        alert(params.titre + '<br><br>' + params.message);
-        ret();
-    };
+  function exDebugger(fnt, params, db, ret) {
+    debugger;
+  }
 
-    function exDebugger(fnt, params, db, ret) {
+  function exGetVersion(fnt, params, db, ret) {
+    ret(exRevision);
+  }
 
-        debugger;
-    };
+  function exPlus(fnt, params, db, ret) {
+    ret(params.A + params.B);
+  }
 
-    function exGetVersion(fnt, params, db, ret) {
+  function exReverse(fnt, params, db, ret) {
+    var i = params.nids.slice();
+    i.reverse();
+    ret(i);
+  }
 
-        ret(exRevision);
-    };
+  function exFilesList(fnt, params, db, ret) {
+    if (params) {
+      var i = [];
+      db.loadFiles(params, function (t, r) {
+        if (t) console.warn(t);
 
-    function exPlus(fnt, params, db, ret) {
-        ret(params.A + params.B);
-    };
-
-    function exReverse(fnt, params, db, ret) {
-        var i = params.nids.slice();
-        i.reverse();
+        if (r) for (var o = 0; o < r.length; o++) i.push(r[o].name);
         ret(i);
-    };
+      });
+    } else ret();
+  }
 
-    function exFilesList(fnt, params, db, ret) {
-        if (params) {
-            var i = [];
-            db.loadFiles(params, (function (t, r) {
-                if (t) console.warn(t);
+  function exJavaScript(fnt, params, db, ret) {
+    var prm = params.args;
 
-                if (r)
-                    for (var o = 0; o < r.length; o++)
-                        i.push(r[o].name);
-                ret(i);
-            }));
+    try {
+      var fn = Function("args", params.code);
+      var Result = fn(params.args);
 
-        } else ret();
-    };
+      ret(Result);
+    } catch (err) {
+      var msgErr =
+        err.message + " à la ligne " + err.line + ", colonne " + err.column;
 
-    function exJavaScript(fnt, params, db, ret) {
-        var prm = params.args;
-
-        try {
-            var fn = Function('args', params.code);
-            var Result = fn(params.args);
-
-            ret(Result);
-        } catch (err) {
-            var msgErr = err.message + ' à la ligne ' + err.line + ', colonne ' + err.column;
-
-            return ret(msgErr);
-        }
-
-    };
-
-    function exComboBox(fnt, params, db, ret) {
-        //debugger;
-        try {
-            //
-            var lst = params.list;
-            var strLines = '';
-            var i;
-            var id = exUtils.generateUniqueId("comboBox");
-
-            var value, label;
-
-            for (i in lst) {
-                if (typeof lst[i] == 'string') {
-                    value = lst[i];
-                    label = lst[i];
-                } else {
-                    value = lst[i].value;
-                    label = lst[i].label;
-                }
-                strLines += '<option id="' + value + '"';
-
-                if (value == params.selected) {
-                    strLines += ' selected';
-                }
-                strLines += '>' + label + '</option>';
-            }
-
-            var codeHTML = '<select class=\'stringeditor\' style=\'height: 100%; width:100%; overflow:visible; padding:0px; background:none; padding-left:5px\' id=' + id +
-                '  onchange=\'window.exFunctions.fireSelectComboBox( "' + id + '","' + params.record._id + '")\'>' +
-                strLines +
-                ' </select>' +
-                '<script>' +
-                'var x = document.getElementById("' + id + '"); ' +
-                'var p = x.parentNode; ' +
-
-                'p.style.overflow = "visible"; p.style.padding = "0px"; p.style.margin = "0px"; p.id = "coucou";' +
-
-                '<\/script>';
-
-            console.log(codeHTML);
-            ret(codeHTML);
-        } catch (err) {
-            var msgErr = err.message + ' à la ligne ' + err.line + ', colonne ' + err.column;
-
-            return ret(msgErr);
-        }
-
-
-    };
-
-
-    function exShareFile(fnt, params, db, ret) {
-
-        //debugger;
-
-        database.shareFile(exUtils.getId(params.record), params.fileName, (function (erreur, link) {
-
-            if (erreur) return ret(erreur);
-
-            ret(link.url);
-
-        }));
-    };
-
-    function exRenameFile(fnt, params, db, ret) {
-
-        database.renameFile(exUtils.getId(params.record), params.oldFileName, params.newFileName, (function (erreur) {
-            if ($.loading(!1), erreur)
-                return $.alert(locale.couldntRenameFile), ret(false);
-
-            ret(true);
-        }));
-
-    };
-
-    function exRemoveFile(fnt, params, db, ret) {
-
-        database.removeFile(exUtils.getId(params.record), params.fileName, (function (erreur) {
-            if ($.loading(!1), erreur)
-                return $.alert(locale.couldntRenameFile), ret(false);
-
-            ret(true);
-        }));
-
-    };
-
-    function exDownloadFile(fnt, params, db, ret) {
-
-        var downloadLink = database.downloadURL(exUtils.getId(params.record), params.fileName);
-        var link = document.createElement('a');
-        link.href = downloadLink;
-        link.download = params.destFileName;
-        link.click();
-        link.remove();
-        console.log("ExDownLoadFile(" + exUtils.getId(params.record) + ", " + params.fileName + ")");
-
-        ret(downloadLink);
-    };
-
-    function exPrompt(fnt, params, db, ret) {
-
-        var result = {};
-        result.value = prompt(params.caption, params.defaultValue);
-        result.ok = (!!result.value);
-
-        ret(result);
-    };
-
-    function exGetComments(fnt, params, db, ret) {
-
-        debugger;
-        database.loadComments(exUtils.getId(params.record), (function (t, i) {
-            var r = [];
-
-            for (c in i) {
-                var o = {
-                    comment: i[c][2],
-                    id: i[c][1],
-                    date: new Date(i[c][0])
-                };
-                r.push(o);
-            };
-            debugger;
-            ret(r);
-        }));
+      return ret(msgErr);
     }
+  }
 
+  function exComboBox(fnt, params, db, ret) {
     //debugger;
-    exFunctions.addExFunction()
-    exFunctions.addExFunction('exAlert', exAlert);
-    exFunctions.addExFunction('exDebugger', exDebugger);
-    exFunctions.addExFunction('exGetVersion', exGetVersion);
-    exFunctions.addExFunction('exPlus', exPlus);
-    exFunctions.addExFunction('exReverse', exReverse);
-    exFunctions.addExFunction('exFilesList', exFilesList);
-    exFunctions.addExFunction('exJavaScript', exJavaScript);
-    exFunctions.addExFunction('exComboBox', exComboBox);
-    exFunctions.addExFunction('exShareFile', exShareFile);
-    exFunctions.addExFunction('exRenameFile', exRenameFile);
-    exFunctions.addExFunction('exRemoveFile', exRemoveFile);
-    exFunctions.addExFunction('exDownloadFile', exDownloadFile);
-    exFunctions.addExFunction('exPrompt', exPrompt);
-    exFunctions.addExFunction('exGetComments', exGetComments);
-    return {}
+    try {
+      //
+      var lst = params.list;
+      var strLines = "";
+      var i;
+      var id = exUtils.generateUniqueId("comboBox");
+
+      var value, label;
+
+      for (i in lst) {
+        if (typeof lst[i] == "string") {
+          value = lst[i];
+          label = lst[i];
+        } else {
+          value = lst[i].value;
+          label = lst[i].label;
+        }
+        strLines += '<option id="' + value + '"';
+
+        if (value == params.selected) {
+          strLines += " selected";
+        }
+        strLines += ">" + label + "</option>";
+      }
+
+      var codeHTML =
+        "<select class='stringeditor' style='height: 100%; width:100%; overflow:visible; padding:0px; background:none; padding-left:5px' id=" +
+        id +
+        "  onchange='window.exFunctions.fireSelectComboBox( \"" +
+        id +
+        '","' +
+        params.record._id +
+        "\")'>" +
+        strLines +
+        " </select>" +
+        "<script>" +
+        'var x = document.getElementById("' +
+        id +
+        '"); ' +
+        "var p = x.parentNode; " +
+        'p.style.overflow = "visible"; p.style.padding = "0px"; p.style.margin = "0px"; p.id = "coucou";' +
+        "</script>";
+
+      console.log(codeHTML);
+      ret(codeHTML);
+    } catch (err) {
+      var msgErr =
+        err.message + " à la ligne " + err.line + ", colonne " + err.column;
+
+      return ret(msgErr);
+    }
+  }
+
+  function exShareFile(fnt, params, db, ret) {
+    //debugger;
+
+    database.shareFile(
+      exUtils.getId(params.record),
+      params.fileName,
+      function (erreur, link) {
+        if (erreur) return ret(erreur);
+
+        ret(link.url);
+      }
+    );
+  }
+
+  function exRenameFile(fnt, params, db, ret) {
+    database.renameFile(
+      exUtils.getId(params.record),
+      params.oldFileName,
+      params.newFileName,
+      function (erreur) {
+        if (($.loading(!1), erreur))
+          return $.alert(locale.couldntRenameFile), ret(false);
+
+        ret(true);
+      }
+    );
+  }
+
+  function exRemoveFile(fnt, params, db, ret) {
+    database.removeFile(
+      exUtils.getId(params.record),
+      params.fileName,
+      function (erreur) {
+        if (($.loading(!1), erreur))
+          return $.alert(locale.couldntRenameFile), ret(false);
+
+        ret(true);
+      }
+    );
+  }
+
+  function exDownloadFile(fnt, params, db, ret) {
+    var downloadLink = database.downloadURL(
+      exUtils.getId(params.record),
+      params.fileName
+    );
+    var link = document.createElement("a");
+    link.href = downloadLink;
+    link.download = params.destFileName;
+    link.click();
+    link.remove();
+    console.log(
+      "ExDownLoadFile(" +
+        exUtils.getId(params.record) +
+        ", " +
+        params.fileName +
+        ")"
+    );
+
+    ret(downloadLink);
+  }
+
+  function exPrompt(fnt, params, db, ret) {
+    var result = {};
+    result.value = prompt(params.caption, params.defaultValue);
+    result.ok = !!result.value;
+
+    ret(result);
+  }
+
+  function exGetComments(fnt, params, db, ret) {
+    debugger;
+    database.loadComments(exUtils.getId(params.record), function (t, i) {
+      var r = [];
+
+      for (c in i) {
+        var o = {
+          comment: i[c][2],
+          id: i[c][1],
+          date: new Date(i[c][0]),
+        };
+        r.push(o);
+      }
+      debugger;
+      ret(r);
+    });
+  }
+
+  function exSetMultiValues(fnt, params, db, ret) {
+    var field;
+    var type;
+
+    // récupération de la table (type) et du champ (field)
+    if (args.record) {
+      type = database.schema.typeOf(args.record._id);
+      field = type.findElement(args.multiField);
+      if (field) {
+        args.record[field.id] = util.multiEncode(args.id);
+        return args.record[field.id];
+      } else
+        return (
+          "Le champ " +
+          args.multiFiled +
+          " esr introuvable dans la table " +
+          type.caption
+        );
+    } else return "Aucun enregistrement définit dans args.record. Essayez {record:this}";
+  }
+
+  //debugger;
+  exFunctions.addExFunction();
+  exFunctions.addExFunction("exAlert", exAlert);
+  exFunctions.addExFunction("exDebugger", exDebugger);
+  exFunctions.addExFunction("exGetVersion", exGetVersion);
+  exFunctions.addExFunction("exPlus", exPlus);
+  exFunctions.addExFunction("exReverse", exReverse);
+  exFunctions.addExFunction("exFilesList", exFilesList);
+  exFunctions.addExFunction("exJavaScript", exJavaScript);
+  exFunctions.addExFunction("exComboBox", exComboBox);
+  exFunctions.addExFunction("exShareFile", exShareFile);
+  exFunctions.addExFunction("exRenameFile", exRenameFile);
+  exFunctions.addExFunction("exRemoveFile", exRemoveFile);
+  exFunctions.addExFunction("exDownloadFile", exDownloadFile);
+  exFunctions.addExFunction("exPrompt", exPrompt);
+  exFunctions.addExFunction("exGetComments", exGetComments);
+  exFunctions.addExFunction("exSetMultiValues", exSetMultiValues);
+
+    return {};
 })();
