d606aef55038f7d81a4f167bfcc2b02a51838e41
JacquesTur <jacques.tur@neuf.fr>
2021-07-06 13:31:11 +0200

    master

--- exFonctions.js
+++ exFonctions.js
@@ -1,5 +1,5 @@
 
-this.exRevision = 'rev 0.00.00.20j';
+this.exRevision = 'rev 0.00.00.20k';
 
 //import ('https://raw.githubusercontent.com/JacquesTur/exFunctionsNinox/master/Modules/exUtils.js');
 //let exUtils = await import('https://raw.githubusercontent.com/JacquesTur/exFunctionsNinox/master/Modules/exUtils.js');
