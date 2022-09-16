9fadef595ac319388fc3290ccdb1aa53373cffe7
JacquesTur <jacques.tur@neuf.fr>
2022-09-16 15:47:49 +0200

    main

--- Jacques/test.js
+++ Jacques/test.js
@@ -1,27 +1,14 @@
-debugger;
-var retour;
-if (args.report && args.records)
-    if (util.isValidFileName(args.fileName))
-        retour = reports.openDesigner({
-            nid : args.id,
-            nids: args.records,
-            reportName: args.report,
-            saveToFileAndClose: function (error, link) {
-                debugger;
-                error && console.log("printAndSaveRecord failed", e),
-                    database.importFileFromURL(args.id, link, args.fileName, (function (erreur) {
-                        debugger;
-                        erreur && console.warn(erreur),
-                            ret(args.id + "/" + args.fileName)
-                    }))
-            }
-        })
-
-    else retour = `Nom de fichier "${args.fileName}" invalide`;
-else retour = `arguments report ou record manquant`;
-debugger;
+var retour = null;
+if (report && records) {
+    retour = reports.openDesigner({
+        nid: id,
+        nids: records,
+        reportName: report,
+        printAllAndClose: true
+    })
+}
+else retour = 'arguments report ou records manquant';
 return retour;
-
 
 
 
@@ -29,15 +16,15 @@ function r(t, i, o, a, r) {
     var s = Date.now(),
         l = n;
     n++,
-    console.log("HTTP Request #" + l, t, i, o, a),
-    e.loading(!0);
+        console.log("HTTP Request #" + l, t, i, o, a),
+        e.loading(!0);
     var c = {
-            body: a || void 0,
-            method: t,
-            url: i,
-            headers: o
-        },
-        d = function(t, i, o) {
+        body: a || void 0,
+        method: t,
+        url: i,
+        headers: o
+    },
+        d = function (t, i, o) {
             e.loading(!1);
             var a = Date.now();
             t ? (console.log("HTTP Response #" + l, a - s + "ms", "ERR", i && i.statusCode, t, o), r({
@@ -61,10 +48,10 @@ function r(t, i, o, a, r) {
             xhrFields: {
                 withCredentials: !(!e.headers || !e.headers.Authorization)
             },
-            success: function(e, i, o) {
+            success: function (e, i, o) {
                 t(void 0, i, e)
             },
-            error: function(e, i, o) {
+            error: function (e, i, o) {
                 t("Request has failed: " + i + " - " + o, i, void 0)
             }
         })
